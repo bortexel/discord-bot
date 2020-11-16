1dd14e0b6292a4f454bfcb9e66779e86f76d2101
RuscalWorld <me@ruscalworld.ru>
2020-11-16 20:54:20 +0300

    И всё-таки эмодзи указывать необязательно

--- src/main/java/ru/bortexel/bot/commands/main/PollCommand.java
+++ src/main/java/ru/bortexel/bot/commands/main/PollCommand.java
@@ -64,7 +64,7 @@ public class PollCommand implements Command {
 
     @Override
     public String getUsage() {
-        return "<вопрос>\n<эмодзи> <название варианта>\n<эмодзи> <название варианта>\n...";
+        return "<вопрос>\n[эмодзи] <название варианта>\n[эмодзи] <название варианта>\n...";
     }
 
     @Override
