1a422b4c477566f26ff4fa3f18dc40946ec213f4
RuscalWorld <me@ruscalworld.ru>
2021-01-06 16:00:50 +0300

    Bortexel4J 1.0.12, получение скриншотов по сезону

--- src/main/java/ru/bortexel/bot/commands/main/PhotoCommand.java
+++ src/main/java/ru/bortexel/bot/commands/main/PhotoCommand.java
@@ -8,9 +8,13 @@ import ru.bortexel.bot.core.AccessLevel;
 import ru.bortexel.bot.core.Command;
 import ru.bortexel.bot.util.Channels;
 import ru.bortexel.bot.util.EmbedUtil;
+import ru.bortexel.bot.util.TextUtil;
 import ru.ruscalworld.bortexel4j.Bortexel4J;
 import ru.ruscalworld.bortexel4j.models.photo.Photo;
+import ru.ruscalworld.bortexel4j.models.season.Season;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class PhotoCommand implements Command {
@@ -22,30 +26,45 @@ public class PhotoCommand implements Command {
 
     @Override
     public void onCommand(Message message) {
-        Photo.getAll(bot.getApiClient()).executeAsync(photos -> {
+        String[] args = TextUtil.getCommandArgs(message);
+        int seasonId = 0;
+
+        if (args.length >= 2) {
             try {
-                MessageChannel channel = message.getChannel();
-                if (photos == null || photos.size() == 0) {
-                    EmbedBuilder builder = EmbedUtil.makeError("Неизвестная ошибка", "Не удалось получить скриншоты с сайта");
-                    channel.sendMessage(builder.build()).queue();
-                    return;
-                }
+                seasonId = Integer.parseInt(args[1]);
+            } catch (Exception ignored) { }
+        }
 
-                Random random = new Random();
-                int index = random.nextInt(photos.size());
-                Photo photo = photos.get(index);
-
-                EmbedBuilder builder = EmbedUtil.makeDefaultEmbed();
-                builder.setImage(photo.getUrl());
-                builder.addField("Сезон", "" + photo.getSeason(), true);
-                if (photo.getDescription() != null) builder.addField("Описание", photo.getDescription(), true);
-                if (photo.getAuthor() != null) builder.addField("Автор", photo.getAuthor(), true);
-
-                channel.sendMessage(builder.build()).queue();
-            } catch (Exception e) {
-                BortexelBot.handleException(e);
-            }
+        if (seasonId == 0) {
+            Photo.getAll(bot.getApiClient()).executeAsync(photos -> handlePhotos(photos, message.getChannel()));
+        } else Season.getByID(seasonId, bot.getApiClient()).executeAsync(season -> {
+            if (season == null) handlePhotos(Collections.emptyList(), message.getChannel());
+            if (season != null) season.getPhotos(bot.getApiClient()).executeAsync(seasonPhotos -> handlePhotos(seasonPhotos.getPhotos(), message.getChannel()));
         });
+    }
+
+    private void handlePhotos(List<Photo> photos, MessageChannel channel) {
+        try {
+            if (photos == null || photos.size() == 0) {
+                EmbedBuilder builder = EmbedUtil.makeError("Неизвестная ошибка", "Не удалось получить скриншоты с сайта");
+                channel.sendMessage(builder.build()).queue();
+                return;
+            }
+
+            Random random = new Random();
+            int index = random.nextInt(photos.size());
+            Photo photo = photos.get(index);
+
+            EmbedBuilder builder = EmbedUtil.makeDefaultEmbed();
+            builder.setImage(photo.getUrl());
+            builder.addField("Сезон", "" + photo.getSeason(), true);
+            if (photo.getDescription() != null) builder.addField("Описание", photo.getDescription(), true);
+            if (photo.getAuthor() != null) builder.addField("Автор", photo.getAuthor(), true);
+
+            channel.sendMessage(builder.build()).queue();
+        } catch (Exception e) {
+            BortexelBot.handleException(e);
+        }
     }
 
     @Override
@@ -60,8 +79,8 @@ public class PhotoCommand implements Command {
 
     @Override
     public String getUsageExample() {
-        return "`$photo` покажет случайный скриншот с сайта\n";
-                //"`$photo 4` покажет случайный скриншот с четвёртого сезона";
+        return "`$photo` покажет случайный скриншот с сайта\n" +
+                "`$photo 4` покажет случайный скриншот с четвёртого сезона";
     }
 
     @Override
