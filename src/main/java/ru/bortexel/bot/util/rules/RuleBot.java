6600d38c7433ca4b0a94561acdf0dac5788bd8bd
RuscalWorld <me@ruscalworld.ru>
2021-02-20 18:34:51 +0300

    Обновлены ссылки на правила

--- src/main/java/ru/bortexel/bot/util/rules/RuleBot.java
+++ src/main/java/ru/bortexel/bot/util/rules/RuleBot.java
@@ -3,12 +3,12 @@ package ru.bortexel.bot.util.rules;
 import java.awt.*;
 
 public class RuleBot {
-    public static final String MAIN_RULES_URL = "https://raw.githubusercontent.com/Bortexel/BortexelRules/master/main.md";
-    public static final String ROLEPLAY_RULES_URL = "https://raw.githubusercontent.com/Bortexel/BortexelRules/master/roleplay.md";
-    public static final String PLACES_RULES_URL = "https://raw.githubusercontent.com/Bortexel/BortexelRules/master/places.md";
-    public static final String MAIN_RULES_TITLE_URL = "https://bortexel.ru/rules/rules-main.png";
-    public static final String ROLEPLAY_RULES_TITLE_URL = "https://bortexel.ru/rules/rules-rp.png";
-    public static final String PLACES_RULES_TITLE_URL = "https://bortexel.ru/rules/rules-places.png";
+    public static final String MAIN_RULES_URL = "https://raw.githubusercontent.com/Bortexel/WebsiteContent/rules/main.md";
+    public static final String ROLEPLAY_RULES_URL = "https://raw.githubusercontent.com/Bortexel/WebsiteContent/rules/roleplay.md";
+    public static final String PLACES_RULES_URL = "https://raw.githubusercontent.com/Bortexel/WebsiteContent/rules/places.md";
+    public static final String MAIN_RULES_TITLE_URL = "https://raw.githubusercontent.com/Bortexel/WebsiteContent/rules/rules-main.png";
+    public static final String ROLEPLAY_RULES_TITLE_URL = "https://raw.githubusercontent.com/Bortexel/WebsiteContent/rules/rules-rp.png";
+    public static final String PLACES_RULES_TITLE_URL = "https://raw.githubusercontent.com/Bortexel/WebsiteContent/rules/rules-places.png";
 
     public static final Color MAIN_RULES_COLOR = Color.decode("#FF6666");
     public static final Color ROLEPLAY_RULES_COLOR = Color.decode("#FF8264");
