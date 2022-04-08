c76bd225d00b1837615d62f005ee2540a9e7475b
RuscalWorld <me@ruscalworld.ru>
2022-04-09 00:01:24 +0300

    Изменять никнейм участника при привязке аккаунта в Discord

--- src/main/java/ru/bortexel/bot/listeners/bortexel/DiscordLinkListener.java
+++ src/main/java/ru/bortexel/bot/listeners/bortexel/DiscordLinkListener.java
@@ -3,6 +3,9 @@ package ru.bortexel.bot.listeners.bortexel;
 import ru.bortexel.bot.BortexelBot;
 import ru.bortexel.bot.util.RoleChecker;
 import ru.ruscalworld.bortexel4j.listening.events.account.GenericDiscordLinkEvent;
+import ru.ruscalworld.bortexel4j.models.user.User;
+
+import java.util.List;
 
 public class DiscordLinkListener extends BotListener {
     public DiscordLinkListener(BortexelBot bot) {
@@ -12,6 +15,16 @@ public class DiscordLinkListener extends BotListener {
     @Override
     public void onAccountDiscordLinked(GenericDiscordLinkEvent event) {
         RoleChecker.updateRoles(event.getDiscord().getDiscordID());
+
+        event.getAccount().getUsers(this.getBot().getApiClient()).executeAsync(accountUsers -> {
+            List<User> users = accountUsers.getUsers();
+            if (users.size() == 0) return;
+
+            this.getBot().getMainGuild().retrieveMemberById(event.getDiscord().getDiscordID()).queue(member -> {
+                if (member == null) return;
+                member.modifyNickname(users.get(0).getUsername()).queue();
+            });
+        });
     }
 
     @Override
