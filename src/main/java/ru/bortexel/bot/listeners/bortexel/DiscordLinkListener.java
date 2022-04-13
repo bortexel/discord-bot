79b34f86069ce91dc0c5a044a0ad17580fa5651c
RuscalWorld <me@ruscalworld.ru>
2022-04-13 11:39:16 +0300

    Обновлять никнейм при входе на сервер

--- src/main/java/ru/bortexel/bot/listeners/bortexel/DiscordLinkListener.java
+++ src/main/java/ru/bortexel/bot/listeners/bortexel/DiscordLinkListener.java
@@ -1,11 +1,8 @@
 package ru.bortexel.bot.listeners.bortexel;
 
 import ru.bortexel.bot.BortexelBot;
-import ru.bortexel.bot.util.RoleChecker;
+import ru.bortexel.bot.util.MemberSynchronizer;
 import ru.ruscalworld.bortexel4j.listening.events.account.GenericDiscordLinkEvent;
-import ru.ruscalworld.bortexel4j.models.user.User;
-
-import java.util.List;
 
 public class DiscordLinkListener extends BotListener {
     public DiscordLinkListener(BortexelBot bot) {
@@ -14,21 +11,11 @@ public class DiscordLinkListener extends BotListener {
 
     @Override
     public void onAccountDiscordLinked(GenericDiscordLinkEvent event) {
-        RoleChecker.updateRoles(event.getDiscord().getDiscordID());
-
-        event.getAccount().getUsers(this.getBot().getApiClient()).executeAsync(accountUsers -> {
-            List<User> users = accountUsers.getUsers();
-            if (users.size() == 0) return;
-
-            this.getBot().getMainGuild().retrieveMemberById(event.getDiscord().getDiscordID()).queue(member -> {
-                if (member == null) return;
-                member.modifyNickname(users.get(0).getUsername()).queue();
-            });
-        });
+        MemberSynchronizer.synchronize(event.getDiscord().getDiscordID());
     }
 
     @Override
     public void onAccountDiscordUnlinked(GenericDiscordLinkEvent event) {
-        RoleChecker.updateRoles(event.getDiscord().getDiscordID());
+        MemberSynchronizer.synchronize(event.getDiscord().getDiscordID());
     }
 }
